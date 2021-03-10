cc056adc0b0ea55b9d9c78f044a4cc5aec9eddd6
Noé Iván Vivero Guzmán <NoeAVGN@outlook.com>
2021-03-09 19:47:19 -0600

    Design SignIn Screen

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -4,29 +4,29 @@ import { PRODUCT_DETAILS_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS 
 
 export const listProducts = () => async(dispatch) => {
     dispatch({
-        type:PRODUCT_LIST_REQUEST
+        type: PRODUCT_LIST_REQUEST
     });
-    try{
+    try {
         const { data } = await Axios.get('/api/products');
-        dispatch({type:PRODUCT_LIST_SUCCESS, payload:data})
-    }catch(error){
-        dispatch({type:PRODUCT_LIST_FAIL,payload:error.message})
+        dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data })
+    } catch (error) {
+        console.log(error)
+        dispatch({ type: PRODUCT_LIST_FAIL, payload: error.message })
     }
 }
 
 export const detailsProduct = (productId) => async(dispatch) => {
     dispatch({
-        type:PRODUCT_DETAILS_REQUEST,
+        type: PRODUCT_DETAILS_REQUEST,
         payload: productId
     });
-    try{
+    try {
         const { data } = await Axios.get(`/api/products/${productId}`);
-        dispatch({type:PRODUCT_DETAILS_SUCCESS, payload:data})
-    }catch(error){
+        dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data })
+    } catch (error) {
         dispatch({
-            type:PRODUCT_DETAILS_FAIL,
-            payload: 
-                error.response && error.response.data.message?
+            type: PRODUCT_DETAILS_FAIL,
+            payload: error.response && error.response.data.message ?
                 error.response.data.message : error.message
         })
     }
